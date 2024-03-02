a5710c69c5b95cecf9cc83e743ef6f2c41af41e8
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2024-03-02 15:34:31 +0100

    fix issue with debug name not removing (Clone)

--- Runtime/Components/NetworkObjectDebugName.cs
+++ Runtime/Components/NetworkObjectDebugName.cs
@@ -14,21 +14,9 @@ namespace CodeSmile.Netcode.Components
 		         "if the object isn't prefixed accordingly.")]
 		[SerializeField] private Boolean m_ReplaceNetworkPrefix;
 
-		[NonSerialized] protected String m_Name;
-
-		private void Awake()
-		{
-#if DEVELOPMENT_BUILD || UNITY_EDITOR
-			m_Name = name.Replace("(Clone)", "");
-#else
-			enabled = false;
-#endif
-		}
-
 		public override void OnNetworkSpawn()
 		{
 			base.OnNetworkSpawn();
-
 			SetDebugName();
 		}
 
@@ -37,15 +25,20 @@ namespace CodeSmile.Netcode.Components
 		protected virtual void SetDebugName()
 		{
 #if DEVELOPMENT_BUILD || UNITY_EDITOR
-			var objName = m_Name;
+			name = GetDebugName(name);
+#endif
+		}
+
+		protected virtual string GetDebugName(string originalName)
+		{
+			var objName = originalName.Replace("(Clone)", "");
 
 			// the extra characters are meant to keep the original string length
 			if (m_ReplaceNetworkPrefix && objName.StartsWith("Network"))
 				objName = $"{(IsOwner ? ">Local " : "<Remote")}{objName.Substring("Network".Length)}";
 
 			var owner = IsOwner ? "(OWNER)" : "";
-			name = $"[{OwnerClientId}|{NetworkObjectId}] {objName} {owner}";
-#endif
+			return $"[{OwnerClientId}|{NetworkObjectId}] {objName} {owner}";
 		}
 	}
 }
