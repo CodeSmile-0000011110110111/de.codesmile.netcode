297a999b76bd34aefc7d2f063439b2be2c70fc62
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2024-03-05 18:45:05 +0100

    added networkdespawntimer

---
+++ Runtime/Components/NetworkDespawnTimer.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (C) 2021-2024 Steffen Itterheim
+// Refer to included LICENSE file for terms and conditions.
+
+using System;
+using System.Collections;
+using Unity.Netcode;
+using UnityEngine;
+
+namespace CodeSmile.Netcode.Components
+{
+	public class NetworkDespawnTimer : NetworkBehaviour
+	{
+		[SerializeField] private Single m_SecondsTillDespawn = 3f;
+
+		private Single m_TargetTime;
+
+		public override void OnNetworkSpawn()
+		{
+			base.OnNetworkSpawn();
+
+			if (IsOwner)
+				StartCoroutine(DespawnWhenTimeOut());
+		}
+
+		private IEnumerator DespawnWhenTimeOut()
+		{
+			m_TargetTime = Time.time + m_SecondsTillDespawn;
+
+			yield return new WaitUntil(() => Time.time > m_TargetTime);
+
+			GetComponent<NetworkObject>().Despawn();
+		}
+	}
+}
