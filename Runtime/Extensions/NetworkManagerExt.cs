0e736fffb1e701e2d4505d6e3774de678d29cdbf
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2024-03-15 13:13:02 +0100

    helper to subscribe to NetworkManager internal OnSingletonReady event

--- Runtime/Extensions/NetworkManagerExt.cs
+++ Runtime/Extensions/NetworkManagerExt.cs
@@ -3,22 +3,29 @@
 
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace CodeSmile.Netcode.Extensions
 {
 	public static class NetworkManagerExt
 	{
-		private static List<Action> s_SingletonReadyCallbacks;
+		private static List<Action> OnSingletonReadyCallbacks;
 
-		public static Boolean IsServerOrHost(this NetworkManager netMan) => netMan.IsServer || netMan.IsHost;
-
+		/// <summary>
+		///     Shorthand for calling: NetworkManager.Singleton.GetComponent<UnityTransport>()
+		/// </summary>
+		/// <param name="netMan"></param>
+		/// <returns>The UnityTransport component.</returns>
 		public static UnityTransport GetTransport(this NetworkManager netMan) => netMan.GetComponent<UnityTransport>();
 
 		/// <summary>
-		///     Invokes the callback if or when the NetworkManager.Singleton has been assigned. Relies on the NetworkManager's
-		///     internal event Action OnSingletonReady to be made public by editing the NetworkManager.cs (v1.8+).
+		///     Let's you subscribe to NetworkManager's internal OnSingletonReady event.
+		///     Will continue to work even if OnSingletonReady may become public in the future.
 		/// </summary>
 		/// <example>
 		///     Usage: call this in either the Awake or OnEnable method. The callback should only be used to subscribe to
@@ -37,26 +44,64 @@ namespace CodeSmile.Netcode.Extensions
 		/// <param name="callback">The action to be invoked when NetworkManager.Singleton has been assigned.</param>
 		public static void InvokeWhenSingletonReady(Action callback)
 		{
+			if (callback == null)
+				throw new ArgumentNullException(nameof(callback));
+
+			// singleton already initialized? => direct Invoke
 			if (NetworkManager.Singleton != null)
 			{
-				callback?.Invoke();
+				callback.Invoke();
 				return;
 			}
 
-			s_SingletonReadyCallbacks ??= new List<Action>();
-			s_SingletonReadyCallbacks.Add(callback);
-			NetworkManager.OnSingletonReady -= InvokeSingletonReadyCallbacks;
-			NetworkManager.OnSingletonReady += InvokeSingletonReadyCallbacks;
+			// one-time init of list and singleton ready event
+			if (OnSingletonReadyCallbacks == null)
+			{
+				OnSingletonReadyCallbacks = new List<Action>();
+				Subscribe(GetSingletonReadyEvent(), InvokeSingletonReadyCallbacksOnce);
+			}
+
+			// put it in the queue to be called later
+			OnSingletonReadyCallbacks.Add(callback);
 		}
 
-		private static void InvokeSingletonReadyCallbacks()
+		private static void InvokeSingletonReadyCallbacksOnce()
 		{
-			NetworkManager.OnSingletonReady -= InvokeSingletonReadyCallbacks;
+			Unsubscribe(GetSingletonReadyEvent(), InvokeSingletonReadyCallbacksOnce);
 
-			foreach (var callback in s_SingletonReadyCallbacks)
-				callback?.Invoke();
+			foreach (var callback in OnSingletonReadyCallbacks)
+				callback.Invoke();
 
-			s_SingletonReadyCallbacks = null;
+			OnSingletonReadyCallbacks = null;
 		}
+
+		private static void Subscribe(EventInfo readyEvent, Action eventHandler) =>
+			readyEvent.GetAddMethod(true).Invoke(readyEvent, new Object[] { eventHandler });
+
+		private static void Unsubscribe(EventInfo readyEvent, Action eventHandler) =>
+			readyEvent.GetRemoveMethod(true).Invoke(readyEvent, new Object[] { eventHandler });
+
+		private static EventInfo GetSingletonReadyEvent()
+		{
+			const String ReadyEventName = "OnSingletonReady";
+
+			var readyEvent = typeof(NetworkManager).GetEvent(ReadyEventName, BindingFlags.Static | BindingFlags.NonPublic);
+
+			// try to get the public version, because this may be made public in the future (was internal in Netcode 1.8.1)
+			if (readyEvent == null)
+				readyEvent = typeof(NetworkManager).GetEvent(ReadyEventName, BindingFlags.Static | BindingFlags.Public);
+
+			if (readyEvent == null)
+			{
+				throw new MissingMemberException("NetworkManager does not have the 'OnSingletonReady' event. " +
+				                                 "This may indicate that an unsupported Netcode package version is installed.");
+			}
+
+			return readyEvent;
+		}
+
+#if UNITY_EDITOR
+		[InitializeOnLoadMethod] private static void ResetStaticFields() => OnSingletonReadyCallbacks = null;
+#endif
 	}
 }
